f568ae1454318d4e87a6601764e5ade09d3d4d33
Myron R Schippers Jr <dubtwee@gmail.com>
2025-08-18 15:55:32 -0400

    creating script for file generation and correcting file paths for template generator, testing with stubbed components for Header and Footer content

---
+++ src/components/templates/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import { type FC } from 'react';
+import { Box, Typography } from '@mui/material';
+
+import type { FooterProps } from './types';
+
+const Footer: FC<FooterProps> = ({}) => {
+  return (
+    <Box>
+      <Typography variant="h2" component="h2">
+        Footer
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
---
+++ src/components/templates/Header/Header.tsx
@@ -0,0 +1,16 @@
+import { type FC } from 'react';
+import { Box, Typography } from '@mui/material';
+
+import type { HeaderProps } from './types';
+
+const Header: FC<HeaderProps> = ({}) => {
+  return (
+    <Box>
+      <Typography variant="h2" component="h2">
+        Header
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
